3d5aa649ff40dce76cc014a416672e2f6d644b50
Misael Calvillo Mancilla <misa@homely.mx>
2022-11-29 19:08:30 -0600

    Se agregaron 2 endpoints

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express();
 
 // Endpoints
 app.get('/', (req, res) => {
-  res.send('Hola, bienvenido a mi sitio web!');
+  res.send(`
+    <strong>Hola, bienvenido a mi sitio web!</strong>
+  `);
 });
+
+app.post('/', (req, res) => {
+  res.send('Esto es una petición POST 🚀');
+})
 
 app.listen(3000, () => {
   console.log('El servidor esta vivo !!!');
