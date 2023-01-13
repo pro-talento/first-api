b4e12ed9e531b1a7ce848077a5215d057c669e84
Cristian Fabian Angel Ceballos <97754645+Angel1805@users.noreply.github.com>
2023-01-12 21:35:11 -0500

    first commit

--- src/db.js
+++ src/db.js
@@ -3,35 +3,51 @@ const path = require('path')
 
 function searchSession(sessionId) {
   // Algoritmo para buscar la sesión
-}
-
-function guardarDatos(nuevosDatos) {
-  fs.writeFile('database.txt', nuevosDatos, (err) => {
+  fs.readFile('database.txt','utf-8', (err, data) => {
     if (err) {
       throw err;
     }
+    const parsedData = JSON.parse(data)
+
+    for (let key in parsedData) {
+      console.log(parsedData[key]);
+      if (parsedData[key].length === sessionId){
+      }
+    }
   
-    console.log('Datos guardados exitosamente ✅');
+    console.log(parsedData);
   });
 }
 
-// Leer un archivo (El archivo tiene que existir)
-fs.readFile('database.txt','utf-8', (err, data) => {
-  if (err) {
-    throw err;
-  }
-  const parsedData = JSON.parse(data)
+searchSession(2)
 
-  parsedData[1] = { nombre: "Misael" }
-  parsedData[2] = { nombre: "David" }
-  parsedData[3] = { nombre: "Angel" }
+// function guardarDatos(nuevosDatos) {
+//   fs.writeFile('database.txt', nuevosDatos, (err) => {
+//     if (err) {
+//       throw err;
+//     }
+  
+//     console.log('Datos guardados exitosamente ✅');
+//   });
+// }
 
-  const stringifiedJSON = JSON.stringify(parsedData)
-  guardarDatos(stringifiedJSON)
-});
+// // Leer un archivo (El archivo tiene que existir)
+// fs.readFile('database.txt','utf-8', (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   const parsedData = JSON.parse(data)
 
-searchSession(1);
+//   parsedData[1] = { nombre: "Misael" }
+//   parsedData[2] = { nombre: "David" }
+//   parsedData[3] = { nombre: "Angel" }
 
-module.exports = {
-  searchSession
-}
+//   const stringifiedJSON = JSON.stringify(parsedData)
+//   guardarDatos(stringifiedJSON)
+// });
+
+// searchSession(1);
+
+// module.exports = {
+//   searchSession
+// }
