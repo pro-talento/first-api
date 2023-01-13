65aa3d5cf8cd6eb53724502536c6fb2985e03e5d
Misael Calvillo Mancilla <misa@homely.mx>
2023-01-12 19:30:46 -0600

    Buscar sesion

--- src/db.js
+++ src/db.js
@@ -1,22 +1,37 @@
 const fs = require('fs')
 const path = require('path')
 
+function searchSession(sessionId) {
+  // Algoritmo para buscar la sesión
+}
+
+function guardarDatos(nuevosDatos) {
+  fs.writeFile('database.txt', nuevosDatos, (err) => {
+    if (err) {
+      throw err;
+    }
+  
+    console.log('Datos guardados exitosamente ✅');
+  });
+}
+
 // Leer un archivo (El archivo tiene que existir)
-// fs.readFile('database.txt','utf-8', (err, data) => {
-//   if (err) {
-//     throw err;
-//   }
-//   const parsedData = JSON.parse(data)
-//   console.log(parsedData)
-// });
-
-// Escribir en un archivo (El archivo tiene que existir)
-const data = 'Este es el nuevo contenido del archivo'
-
-fs.writeFile('database.txt', data, (err) => {
+fs.readFile('database.txt','utf-8', (err, data) => {
   if (err) {
     throw err;
   }
+  const parsedData = JSON.parse(data)
 
-  console.log('Datos guardados exitosamente ✅');
+  parsedData[1] = { nombre: "Misael" }
+  parsedData[2] = { nombre: "David" }
+  parsedData[3] = { nombre: "Angel" }
+
+  const stringifiedJSON = JSON.stringify(parsedData)
+  guardarDatos(stringifiedJSON)
 });
+
+searchSession(1);
+
+module.exports = {
+  searchSession
+}
